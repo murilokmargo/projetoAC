18057b6b12f11a1064ac2374227f32af4ab125dd
Murilo Camargo <murilokmargo@gmail.com>
2021-08-24 11:23:22 -0400

    Terceiro commit, melhorias.

--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -3,6 +3,6 @@ class RoomMessage < ApplicationRecord
   belongs_to :room, inverse_of: :room_messages
 
   def as_json(options)
-    super(options).merge(user_avatar_url: user.gravatar_url)
+    super(options).merge(user_avatar_url: user.gravatar_url, user_username: user.username)
   end
 end
